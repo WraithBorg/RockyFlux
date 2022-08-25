cf8d45af8e186953b6d0bad9ea2b2ff700ea78c1
张旭 <2325098522@qq.com>
2022-08-25 21:21:31 +0800

    代码优化

--- readme.md
+++ readme.md
@@ -8,3 +8,7 @@
 ### Test Url
 流式数据展示 `http://127.0.0.1:8889/flux`
 商品列表,流式响应数据 `http://127.0.0.1:8889/good/showGoodDetails`
+http://127.0.0.1:8889/sse
+http://127.0.0.1:8889/good/showGoodDetails
+### Test Download
+`http://127.0.0.1:8889/download3/cldpoint.war`
--- src/main/java/com/dddcs/dddcs/controller/IndexController.java
+++ src/main/java/com/dddcs/dddcs/controller/IndexController.java
@@ -1,5 +1,9 @@
 package com.dddcs.dddcs.controller;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ZeroCopyHttpOutputMessage;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,6 +12,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.io.File;
+import java.io.IOException;
 import java.time.Duration;
 
 /**
@@ -33,4 +39,20 @@ public class IndexController {
     public Mono<String> findById( ) {
         return Mono.just("Mono");
     }
+
+    /**
+     *
+     * @param response
+     * @param filename
+     * @return
+     */
+    @GetMapping("download3/{filename}")
+    public Mono<Void> download (ServerHttpResponse response, @PathVariable String filename) {
+        ZeroCopyHttpOutputMessage zeroCopyResponse = (ZeroCopyHttpOutputMessage) response;
+        response.getHeaders().set(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + filename);
+        response.getHeaders().setContentType(MediaType.APPLICATION_PDF);
+        File file = new File("D:\\zxu_download\\" + filename);
+        return zeroCopyResponse.writeWith(file, 0, file.length());
+    }
+
 }
